c60ef2d37c79b42d79a6c16f13b2a8cf82abc5b4
Vered Bauer <veredbauer@Vereds-MacBook-Air.local>
2014-02-09 18:46:03 -0600

    added columns and model

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -2,11 +2,6 @@ require 'active_record'
 require 'postgres-copy'
 require 'pg_data_encoder'
 require 'benchmark'
-# Create a test db before running
-# add any needed username, password, port
-# install the required gems
-#
-# gem install postgres-copy pg_data_encoder activerecord --no-ri --no-rdoc
 
 
 ActiveSupport.on_load :active_record do
@@ -21,10 +16,10 @@ ActiveRecord::Base.establish_connection(
 ActiveRecord::Base.connection.execute %{
   SET client_min_messages TO warning;
   DROP TABLE IF EXISTS test_models;
-  CREATE TABLE test_models(id serial PRIMARY KEY, data VARCHAR);
+  CREATE TABLE test_models (id serial PRIMARY KEY, data VARCHAR);
 }
 
-class TestModel < ActiveRecord::Base
+class Exam < ActiveRecord::Base
 end
 
 encoder = PgDataEncoder::EncodeForCopy.new
@@ -37,16 +32,7 @@ puts Benchmark.measure {
 }
 puts "inserting into db"
 puts Benchmark.measure {
-  TestModel.pg_copy_from(encoder.get_io, :format => :binary, :columns => [:data])
+  Exam.pg_copy_from(encoder.get_io, :format => :binary, :columns => [:id, :username, :password, :port])
 }
 
 encoder.remove
-# Results on my i5 with ssd backed postgres server
-# 11.7 seconds to generate data file.   3.7 seconds to insert 1,000,000 simple items into a table.
-#
-# 11.670000   0.010000  11.680000 ( 11.733414)
-#  0.030000   0.000000   0.030000 (  3.782371)
-
-
-
-
